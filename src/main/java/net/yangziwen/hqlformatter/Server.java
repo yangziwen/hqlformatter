755e903e2f67e5535a95fa300e3543c1408b2b02
zyang <yzwcroco@gmail.com>
2015-10-25 16:33:24 +0800

    添加一个自动打开浏览器的机制

--- src/main/java/net/yangziwen/hqlformatter/Server.java
+++ src/main/java/net/yangziwen/hqlformatter/Server.java
@@ -1,5 +1,7 @@
 package net.yangziwen.hqlformatter;
 
+import java.util.concurrent.TimeUnit;
+
 import net.yangziwen.hqlformatter.controller.SqlController;
 import spark.Spark;
 
@@ -9,7 +11,7 @@ public class Server {
 	
 	public static void main(String[] args) {
 		
-		int port = getPort();
+		final int port = getPort();
 		
 		Spark.port(port);
 		
@@ -17,7 +19,22 @@ public class Server {
 		
 		Spark.staticFileLocation("/static");
 		
+		// 看上去没有办法获取底层的jettyServer
+		// 因此没法通过LifeCycleLisener来注册开启浏览器的操作
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					TimeUnit.SECONDS.sleep(1L);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				openApp(port);
+			}
+		}).start();
+		
 		initControllers();
+		
 		
 	}
 	
@@ -33,6 +50,25 @@ public class Server {
 		
 		SqlController.init();
 		
+	}
+	
+	private static void openApp(int port) {
+		String url = "http://localhost:" + port;
+		Boolean autoOpen = false;
+		try {
+			autoOpen = Boolean.valueOf(System.getProperty("autoOpen"));
+		} catch (Exception e) {
+		}
+		if(!Boolean.TRUE.equals(autoOpen)) {
+			return;
+		}
+		String cmd = "cmd /c start " + url;
+		try {
+			Process process = Runtime.getRuntime().exec(cmd);
+			process.waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
